c8795e1d81fc97d226a96307f65ea6ef227a75f3
ttsaturyan <you@example.com>
2020-03-12 02:49:45 +0400

    tic tac toe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let td = document.getElementsByTagName("td");
 let x = true;
-let y;
+let xxx = document.getElementById("X");
+let next;
 const array = [
   [0, 1, 2],
   [3, 4, 5],
@@ -11,7 +12,10 @@ const array = [
   [0, 4, 8],
   [2, 4, 6]
 ];
-
+window.onload = function() {
+  document.getElementById("result_X").innerHTML = sessionStorage.getItem("X");
+  document.getElementById("result_O").innerHTML = sessionStorage.getItem("O");
+};
 document.write("<table border=1>");
 for (let i = 0; i < 3; i++) {
   document.write("<tr>");
@@ -24,20 +28,36 @@ document.write("</table>");
 
 for (let index = 0; index < td.length; index++) {
   let element = td[index];
-  element.addEventListener("click", function myFunction() {
-    y = x ? "X" : "O";
-    element.innerHTML = y;
-    for (let i = 0; i < array.length; i++) {
-      for (let e = 0; e < array[i].length; e++) {
-        if (index === array[i][e]) {
-          array[i][e] = y;
-        }
-        if (array[i][0] === "X" && array[i][1] === "X" && array[i][2] === "X") {
-          console.log(array[i][e]);
-          document.location.reload(true);
+  element.addEventListener(
+    "click",
+    function myFunction() {
+      next = x ? "X" : "O";
+      element.innerHTML = next;
+      for (let i = 0; i < array.length; i++) {
+        for (let e = 0; e < array[i].length; e++) {
+          if (index === array[i][e]) {
+            array[i][e] = next;
+          }
+          if (
+            array[i][0] === next &&
+            array[i][1] === next &&
+            array[i][2] === next
+          ) {
+            win(next);
+
+            // document.location.reload(true);
+          }
         }
       }
-    }
-    x = !x;
-  });
+      x = !x;
+    },
+    { once: true }
+  );
+}
+
+function win(next) {
+  sessionStorage.setItem(next, +sessionStorage.getItem(next) + 1);
+  document.getElementById(`result_${next}`).innerHTML = sessionStorage.getItem(
+    next
+  );
 }
